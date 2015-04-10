539c4b73a732f270b9f7870e543d8a4dadc00a6c
Florian Woelfl <flo.woelfl@web.de>
2015-04-10 14:29:15 +0200

    Script to find all 16 GSN stations in my domain from all event folders

---
+++ scripts/find_GSN_stations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+find . -name IU.PMSA.00.BHE* > GSN_stations.txt
+find . -name IU.CASY.00.BHE* >> GSN_stations.txt
+find . -name IU.QSPA.00.BHE* >> GSN_stations.txt
+find . -name GT.SBA.00.BHE* >> GSN_stations.txt
+find . -name GT.VNDA.00.BHE* >> GSN_stations.txt
+find . -name II.HOPE.00.BHE* >> GSN_stations.txt
+find . -name II.EFI.00.BHE* >> GSN_stations.txt
+find . -name GT.PLCA.00.BHE* >> GSN_stations.txt
+find . -name IU.TRQA.00.BHE* >> GSN_stations.txt
+find . -name IU.TRIS.00.BHE* >> GSN_stations.txt
+find . -name II.SUR.00.BHE* >> GSN_stations.txt
+find . -name GT.BOSA.00.BHE* >> GSN_stations.txt
+find . -name GT.LBTB.00.BHE* >> GSN_stations.txt
+find . -name II.TAU.00.BHE* >> GSN_stations.txt
+find . -name IU.NWAO.00.BHE* >> GSN_stations.txt
+find . -name IU.SNZO.00.BHE* >> GSN_stations.txt
