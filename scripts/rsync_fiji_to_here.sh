e9bf4656fe9f0c36acea04ad12118a09aeba3ed0
Florian Woelfl <flo.woelfl@web.de>
2015-04-27 09:58:47 +0200

    Add rsync scripts

---
+++ scripts/rsync_fiji_to_here.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+rsync -aPvh --stats --delete fiji:/export/data/fwoelfl/masterarbeit/LASIF/ LASIF
---
+++ scripts/rsync_here_to_fiji.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+rsync -aPvh --stats --delete LASIF/ fiji:/export/data/fwoelfl/masterarbeit/LASIF
