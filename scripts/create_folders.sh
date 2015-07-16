b1ec2a4ecd72fdd9dfb340b6cadbe528e25c193b
Florian Woelfl <flo.woelfl@web.de>
2015-07-16 23:07:24 +0200

    Adapt the create_folders.sh to create arbitrarily many folders in SPECFEM.

--- scripts/create_folders.sh
+++ scripts/create_folders.sh
@@ -1,11 +1,16 @@
-mkdir run0001 run0002
+#!/bin/bash
+mkdir run0001
 cd run0001
 mkdir OUTPUT_FILES DATA
 ln -s ../DATABASES_MPI .
 cd OUTPUT_FILES
 ln -s ../../OUTPUT_FILES/* .
-cd ../../run0002
-mkdir OUTPUT_FILES DATA
-ln -s ../DATABASES_MPI .
-cd OUTPUT_FILES
-ln -s ../../OUTPUT_FILES/* .
+
+for i in 'seq 2 10';
+do 
+    cd ../../run000$i
+    mkdir OUTPUT_FILES DATA
+    ln -s ../DATABASES_MPI .
+    cd OUTPUT_FILES
+    ln -s ../../OUTPUT_FILES/* .
+done
