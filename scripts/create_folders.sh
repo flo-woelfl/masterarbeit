a4cd580a7c7cb008aa9351ce868cec6b0d021151
flo-woelfl <flo.woelfl@web.de>
2015-07-22 15:26:43 +0200

    Update of the create_folders.sh to make multiple folder creation possible.

--- scripts/create_folders.sh
+++ scripts/create_folders.sh
@@ -2,14 +2,31 @@
 mkdir run0001
 cd run0001
 mkdir OUTPUT_FILES DATA
+cd DATA
+ln -s ../../DATA/cemRequest/ .
+cd ..
 ln -s ../DATABASES_MPI .
 cd OUTPUT_FILES
 ln -s ../../OUTPUT_FILES/* .
 
-for i in 'seq 2 10';
-do 
-    cd ../../run000$i
+cd ../..
+
+mkdir run0010
+cd run0010
+mkdir OUTPUT_FILES DATA
+ln -s ../DATABASES_MPI .
+cd OUTPUT_FILES
+ln -s ../../OUTPUT_FILES/* .
+
+for i in `seq 2 9`;
+do
+    cd ../..
+    mkdir run000$i 
+    cd run000$i
     mkdir OUTPUT_FILES DATA
+    cd DATA
+    ln -s ../../DATA/cemRequest/ .
+    cd ..
     ln -s ../DATABASES_MPI .
     cd OUTPUT_FILES
     ln -s ../../OUTPUT_FILES/* .
